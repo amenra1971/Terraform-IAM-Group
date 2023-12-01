c29e4ede378e340a096043e5895506de570da225
amenra1971 <143852344+amenra1971@users.noreply.github.com>
2023-12-01 14:49:42 -0600

    Add files via upload
    updated group permissions

--- 1-IAMGroup.tf
+++ 1-IAMGroup.tf
@@ -17,3 +17,23 @@ resource "aws_iam_group" "group4" {
   name = "Traditional_Wife"
   path = "/"
 }
+
+resource "aws_iam_group_policy_attachment" "group1-ec2-full-access" {
+  group      = aws_iam_group.group1.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "group2-ec2-full-access" {
+  group      = aws_iam_group.group2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "group3-ec2-full-access" {
+  group      = aws_iam_group.group3.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "group4-ec2-full-access" {
+  group      = aws_iam_group.group4.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
